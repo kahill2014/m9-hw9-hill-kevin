50bd84eaef996e5677c7da1662dc44e1015e2558
Kevin Hill <kahill2014@gmail.com>
2020-07-20 14:26:51 -0500

    Add a basic working example of scrollmagic plugin

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 // Initialize the scrollmagic plugin
 $(document).ready(function() {
+    // init controller
+    var controller = new ScrollMagic.Controller();
 
+    // create a scene
+    var scene = new ScrollMagic.Scene({
+        triggerElement: '#section-2',
+        duration: 500, // the scene should last for a scroll distance of 100px
+        offset: 300 // start this scene after scrolling for 50px
+    })
+        .setPin('#section-2') // pins the element for the the scene's duration
+        .addTo(controller); // assign the scene to the controller
 });
 
